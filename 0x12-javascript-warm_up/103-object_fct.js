e8efc9faacf76b2e7bcf8f19eb83f964826b66d6
Nourddine97 <hassanmaghouzi@gmail.com>
2024-04-04 11:52:19 -0700

    e

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,17 +1,5 @@
 #!/usr/bin/node
-//  new function incr that increments the integer value.
-
-const myObject = {
-  type: 'object',
-  value: 12
+// function that returns the addition of 2 integers.
+exports.add = function (a, b) {
+  return a + b;
 };
-console.log(myObject);
-myObject.incr = function () {
-  myObject.value++;
-};
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
